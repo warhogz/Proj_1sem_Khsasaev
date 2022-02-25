4b6e780cef0cacf845b70615930c940e96f16b17
warhogz <90264787+warhogz@users.noreply.github.com>
2022-02-25 22:48:16 +0300

    added 12 practical work

---
+++ PZ_12/PZ_12_2.py
@@ -0,0 +1,25 @@
+# Разработать программу с применением пакета tk, взяв в качестве условия одну
+# любую задачу из ПЗ №№ 3 – 8.
+# Программа взята из ПЗ_7-2
+# Дана строка, изображающая целое положительное число. Вывести сумму цифр этого числа.
+import tkinter as tk
+
+def words():
+        text1 = str(text.get())
+        textLen = len(text1)
+        num = 0
+        if textLen > 0:
+            for i in range(1, textLen):
+                if (text1[i]) == ' ' and text1[i - 1] != ' ':
+                    num += 1
+        qq.config(text="Количество слов в строке: {}".format(num + 1))
+
+root = tk.Tk()
+root.geometry('200x80')
+text = tk.Entry(root, width = 30)
+text.pack()
+qq = tk.Label(root, text="Введите слова")
+qq.pack()
+button = tk.Button(root, text='Нажать', command=words)
+button.pack()
+root.mainloop()
