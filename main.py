46806afceeb65aee0f1bd0c5190287238c08326c
warhogz <90264787+warhogz@users.noreply.github.com>
2022-04-09 12:49:03 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import figures
+
+print(figures.circle_area())
+print(figures.circle_perimeter())
+print(figures.square_perimeter())
+print(figures.square_area())
+print(figures.triangle_area())
+print(figures.triangle_perimeter())
