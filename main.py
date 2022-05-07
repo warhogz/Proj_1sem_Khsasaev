a71916382b148a2aa28fdeb2ad574eb57352e7bc
warhogz <90264787+warhogz@users.noreply.github.com>
2022-05-07 17:04:11 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import figures
-
-print(figures.circle_area())
-print(figures.circle_perimeter())
-print(figures.square_perimeter())
-print(figures.square_area())
-print(figures.triangle_area())
-print(figures.triangle_perimeter())
